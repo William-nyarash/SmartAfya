62b262a663958b3d67457b35cddf17916bf522c0
william nyaranga <owetywilliamnyaranga@gmail.com>
2026-02-28 01:04:15 +0000

    fix

--- AfyaBora/src/server.js
+++ AfyaBora/src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import diagnosis from "./service/service.js";
+import cors from "cors";
+
+
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.get('/', (req, res)=> {
     res.send("hello world")
 })
